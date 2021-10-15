043cbc0cc0e93826f66e70eb6fb45cb86d42e749
Vincent Manzoni <manzovince@vinces-macbook-pro.home>
2021-10-15 11:05:41 +0200

    +bookmark folders, tags and urls styles +data-bookmark.json

---
+++ data-bookmarks.js
@@ -0,0 +1,169 @@
+var dataBookmarks = {
+    "tags": ["Design", "Tool", "Video", "Watch", "Todo", "Transport", "Science", "Journal", "Physics", "Astronomy", "Biology", "Neurosciences"],
+    "bookmarks": [{
+            "title": "Figma",
+            "url": "https://figma.com",
+            "tags": ["Design", "Tool"]
+        },
+        {
+            "title": "Youtube",
+            "url": "https://youtube.com",
+            "tags": ["Video", "Watch"]
+        },
+        {
+            "title": "Navigo annuel",
+            "url": "https://www.iledefrance-mobilites.fr/titres-et-tarifs/detail/forfait-navigo-annuel",
+            "tags": ["Todo", "💸", "Transport"]
+        },
+        {
+            "title": "Quanta Magazine",
+            "url": "https://www.quantamagazine.org",
+            "tags": ["Science", "Journal", "Mathematics", "Physics"]
+        },
+        {
+            "title": "XZ/fonts",
+            "url": "https://docs.xz.style/fonts",
+            "tags": ["Tool", "Design", "Fonts"]
+        },
+        {
+            "title": "Number.dev",
+            "url": "https://numbr.dev",
+            "tags": ["Mathematics", "Tool"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+        {
+            "title": "AAAAAA",
+            "url": "BBBBB",
+            "tags": ["CCC", "DDD", "EEE"]
+        },
+    ]
+}
--- scripts.js
+++ scripts.js
@@ -6,32 +6,20 @@
 // Load functions
 //========================
 
-window.onload = function () {
+window.onload = function() {
     updateTime();
     updateDate();
 
     document.querySelector('#input').focus();
 
-    document.querySelector('body').addEventListener('click', () => {
+    document.querySelector('main').addEventListener('click', () => {
         document.querySelector('#input').focus();
     });
 
     document.onkeydown = handleKeyDown;
-    UnsplashBg();
+    displayTags();
+    displayURLs();
 };
-
-//========================
-// Header Background
-//========================
-
-// Unsplash Image Background
-function UnsplashBg() {
-    fetch(`https://source.unsplash.com/1600x900/?space,nature,sea,mountain,sky`).then((response) => {
-        document.getElementById("header").src = `${response.url}`;
-        console.log(response.url);
-        //document.getElementById("header").backgroundImage = "url('" + response.url + "')";
-    })
-}
 
 //========================
 // Date and time
@@ -45,29 +33,19 @@ function updateTime() {
     hour = checkTime(today.getHours());
     minutes = checkTime(today.getMinutes());
     document.getElementById('time').innerHTML = hour + '<span class="blink">·</span>' + minutes;
-    t = setTimeout(function () { updateTime() }, 5900);
-}
-
-function randomEmoji() {
-    var emojis = [
-        '😄','😃','😀','😊','☺','😉','😍','😘','😚','😗','😙','😜','😝','😛','😳','😁','😔','😌','😒','😞','😣','😢','😂','😭','😪','😥','😰','😅','😓','😩','😫','😨','😱','😠','😡','😤','😖','😆','😋','😷','😎','😴','😵','😲','😟','😦','😧','😈','👿','😮','😬','😐','😕','😯','😶','😇','😏','😑','👲','👳','👮','👷','💂','👶','👦','👧','👨','👩','👴','👵','👱','👼','👸','😺','😸','😻','😽','😼','🙀','😿','😹','😾','👹','👺','🙈','🙉','🙊','💀','👽','💩','🔥','✨','🌟','💫','💥','💢','💦','💧','💤','💨','👂','👀','👃','👅','👄','👍','👎','👌','👊','✊','✌','👋','✋','👐','👆','👇','👉','👈','🙌','🙏','☝','👏','💪','🚶','🏃','💃','👫','👪','👬','👭','💏','💑','👯','🙆','🙅','💁','🙋','💆','💇','💅','👰','🙎','🙍','🙇','🎩','👑','👒','👟','👞','👡','👠','👢','👕','👔','👚','👗','🎽','👖','👘','👙','💼','👜','👝','👛','👓','🎀','🌂','💄','💛','💙','💜','💚','❤','💔','💗','💓','💕','💖','💞','💘','💌','💋','💍','💎','👤','👥','💬','👣','💭','🐶','🐺','🐱','🐭','🐹','🐰','🐸','🐯','🐨','🐻','🐷','🐽','🐮','🐗','🐵','🐒','🐴','🐑','🐘','🐼','🐧','🐦','🐤','🐥','🐣','🐔','🐍','🐢','🐛','🐝','🐜','🐞','🐌','🐙','🐚','🐠','🐟','🐬','🐳','🐋','🐄','🐏','🐀','🐃','🐅','🐇','🐉','🐎','🐐','🐓','🐕','🐖','🐁','🐂','🐲','🐡','🐊','🐫','🐪','🐆','🐈','🐩','🐾','💐','🌸','🌷','🍀','🌹','🌻','🌺','🍁','🍃','🍂','🌿','🌾','🍄','🌵','🌴','🌲','🌳','🌰','🌱','🌼','🌐','🌞','🌝','🌚','🌑','🌒','🌓','🌔','🌕','🌖','🌗','🌘','🌜','🌛','🌙','🌍','🌎','🌏','🌋','🌌','🌠','⭐','☀','⛅','☁','⚡','☔','❄','⛄','🌀','🌁','🌈','🌊','🎍','💝','🎎','🎒','🎓','🎏','🎆','🎇','🎐','🎑','🎃','👻','🎅','🎄','🎁','🎋','🎉','🎊','🎈','🎌','🔮','🎥','📷','📹','📼','💿','📀','💽','💾','💻','📱','☎','📞','📟','📠','📡','📺','📻','🔊','🔉','🔈','🔇','🔔','🔕','📢','📣','⏳','⌛','⏰','⌚','🔓','🔒','🔏','🔐','🔑','🔎','💡','🔦','🔆','🔅','🔌','🔋','🔍','🛁','🛀','🚿','🚽','🔧','🔩','🔨','🚪','🚬','💣','🔫','🔪','💊','💉','💰','💴','💵','💷','💶','💳','💸','📲','📧','📥','📤','✉','📩','📨','📯','📫','📪','📬','📭','📮','📦','📝','📄','📃','📑','📊','📈','📉','📜','📋','📅','📆','📇','📁','📂','✂','📌','📎','✒','✏','📏','📐','📕','📗','📘','📙','📓','📔','📒','📚','📖','🔖','📛','🔬','🔭','📰','🎨','🎬','🎤','🎧','🎼','🎵','🎶','🎹','🎻','🎺','🎷','🎸','👾','🎮','🃏','🎴','🀄','🎲','🎯','🏈','🏀','⚽','⚾','🎾','🎱','🏉','🎳','⛳','🚵','🚴','🏁','🏇','🏆','🎿','🏂','🏊','🏄','🎣','☕','🍵','🍶','🍼','🍺','🍻','🍸','🍹','🍷','🍴','🍕','🍔','🍟','🍗','🍖','🍝','🍛','🍤','🍱','🍣','🍥','🍙','🍘','🍚','🍜','🍲','🍢','🍡','🍳','🍞','🍩','🍮','🍦','🍨','🍧','🎂','🍰','🍪','🍫','🍬','🍭','🍯','🍎','🍏','🍊','🍋','🍒','🍇','🍉','🍓','🍑','🍈','🍌','🍐','🍍','🍠','🍆','🍅','🌽','🏠','🏡','🏫','🏢','🏣','🏥','🏦','🏪','🏩','🏨','💒','⛪','🏬','🏤','🌇','🌆','🏯','🏰','⛺','🏭','🗼','🗾','🗻','🌄','🌅','🌃','🗽','🌉','🎠','🎡','⛲','🎢','🚢','⛵','🚤','🚣','⚓','🚀','✈','💺','🚁','🚂','🚊','🚉','🚞','🚆','🚄','🚅','🚈','🚇','🚝','🚋','🚃','🚎','🚌','🚍','🚙','🚘','🚗','🚕','🚖','🚛','🚚','🚨','🚓','🚔','🚒','🚑','🚐','🚲','🚡','🚟','🚠','🚜','💈','🚏','🎫','🚦','🚥','⚠','🚧','🔰','⛽','🏮','🎰','♨','🗿','🎪','🎭','📍','🚩','⬆','⬇','⬅','➡','🔠','🔡','🔤','↗','↖','↘','↙','↔','↕','🔄','◀','▶','🔼','🔽','↩','↪','ℹ','⏪','⏩','⏫','⏬','⤵','⤴','🆗','🔀','🔁','🔂','🆕','🆙','🆒','🆓','🆖','📶','🎦','🈁','🈯','🈳','🈵','🈴','🈲','🉐','🈹','🈺','🈶','🈚','🚻','🚹','🚺','🚼','🚾','🚰','🚮','🅿','♿','🚭','🈷','🈸','🈂','Ⓜ','🛂','🛄','🛅','🛃','🉑','㊙','㊗','🆑','🆘','🆔','🚫','🔞','📵','🚯','🚱','🚳','🚷','🚸','⛔','✳','❇','❎','✅','✴','💟','🆚','📳','📴','🅰','🅱','🆎','🅾','💠','➿','♻','♈','♉','♊','♋','♌','♍','♎','♏','♐','♑','♒','♓','⛎','🔯','🏧','💹','💲','💱','©','®','™','〽','〰','🔝','🔚','🔙','🔛','🔜','❌','⭕','❗','❓','❕','❔','🔃','🕛','🕧','🕐','🕜','🕑','🕝','🕒','🕞','🕓','🕟','🕔','🕠','🕕','🕖','🕗','🕘','🕙','🕚','🕡','🕢','🕣','🕤','🕥','🕦','✖','➕','➖','➗','♠','♥','♣','♦','💮','💯','✔','☑','🔘','🔗','➰','🔱','🔲','🔳','◼','◻','◾','◽','▪','▫','🔺','⬜','⬛','⚫','⚪','🔴','🔵','🔻','🔶','🔷','🔸','🔹'
-    ];
-    const random = Math.floor(Math.random() * emojis.length);
-    console.log(random, emojis[random]);
-    return (emojis[random]);
+    t = setTimeout(function() { updateTime() }, 5900);
 }
 
 function updateDate() {
     var today = new Date();
     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var emoji = randomEmoji();
 
     day = today.getDay();
     num = today.getDate();
     month = today.getMonth();
 
-    document.getElementById('date').innerHTML = days[day] + ", " + num + " " + months[month] + " " + emoji;
+    document.getElementById('date').innerHTML = days[day] + ", " + num + " " + months[month];
 }
 
 //========================
@@ -103,9 +81,9 @@ function redirect(url) {
 
 // Adds protocol if not present, encodes search string
 function buildURL(url, search = '', query = '') {
-    let dest = (/(http(s)?:\/\/.)/.test(url))
-        ? url
-        : 'http://' + url;
+    let dest = (/(http(s)?:\/\/.)/.test(url)) ?
+        url :
+        'http://' + url;
     return dest + search + encodeURIComponent(query);
 }
 
@@ -157,13 +135,9 @@ function evaluateInput() {
     // Execute
     if (isURL) {
         redirect(buildURL(command));
-    }
-
-    else if (validCommand) {
+    } else if (validCommand) {
         commands[command](args);
-    }
-
-    else {
+    } else {
         commands[['g']](args);
     }
 
@@ -179,6 +153,8 @@ let aliases = {
     // alias: command
     'cal': 'gc',
     'gk': 'k',
+    'gm': 'map',
+    'git': 'gh',
     'ddg': 'dg',
     '?': 'help'
 }
@@ -186,21 +162,32 @@ let aliases = {
 const commands = {
     // Google
     'g': (args) => {
-        const url = 'https://google.com', search = '/search?q=';
+        const url = 'https://google.com',
+            search = '/search?q=';
         if (args.length == 0) redirect(url)
         else redirect(buildURL(url, search, args));
     },
 
     // DuckDuckGo
     'dg': (args) => {
-        const url = 'https://duckduckgo.com', search = '/?q=';
+        const url = 'https://duckduckgo.com',
+            search = '/?q=';
+        if (args.length == 0) redirect(url)
+        else redirect(buildURL(url, search, args));
+    },
+
+    // Qwant
+    'qw': (args) => {
+        const url = 'https://www.qwant.com',
+            search = '/?l=fr&q=';
         if (args.length == 0) redirect(url)
         else redirect(buildURL(url, search, args));
     },
 
     // Reddit
     'r': (args) => {
-        const url = 'https://reddit.com', search = '/r/';
+        const url = 'https://reddit.com',
+            search = '/r/';
         let query = (args.length > 0) ? args[0] : '';
 
         const validSort = (sort) => {
@@ -216,29 +203,29 @@ const commands = {
                 redirect(url);
                 break;
 
-            // Given a subreddit
+                // Given a subreddit
             case 1:
                 redirect(buildURL(url, search, args[0]));
                 break;
 
-            // Given subreddit and sort
+                // Given subreddit and sort
             case 2:
-                query += (validSort(args[1]))
-                    ? '/' + args[1]
-                    : '';
+                query += (validSort(args[1])) ?
+                    '/' + args[1] :
+                    '';
                 redirect(url + search + query);
                 break;
 
-            // Given subreddit, sort and range
+                // Given subreddit, sort and range
             case 3:
                 if (['top', 'controversial'].includes(args[1])) {
-                    query += (validRange(args[2]))
-                        ? '/' + args[1] + '?t=' + args[2]
-                        : '';
+                    query += (validRange(args[2])) ?
+                        '/' + args[1] + '?t=' + args[2] :
+                        '';
                 } else {
-                    query += (validSort(args[1]))
-                        ? '/' + args[1]
-                        : '';
+                    query += (validSort(args[1])) ?
+                        '/' + args[1] :
+                        '';
                 }
                 redirect(url + search + query);
                 break;
@@ -281,7 +268,8 @@ const commands = {
 
     // Youtube
     'y': (args) => {
-        const url = 'https://youtube.com', search = '/results?search_query=';
+        const url = 'https://youtube.com',
+            search = '/results?search_query=';
         if (args.length == 0) {
             redirect(url);
         } else {
@@ -294,56 +282,64 @@ const commands = {
 
     // Wikipedia
     'w': (args) => {
-        const url = 'https://wikipedia.org', search = '/w/index.php?title=Special:Search&search=';
+        const url = 'https://wikipedia.org',
+            search = '/w/index.php?title=Special:Search&search=';
         if (args.length == 0) redirect(url)
         else redirect(buildURL(url, search, args.join(' ')));
     },
 
     // GitHub
     'gh': (args) => {
-        const url = 'https://github.com', search = '/';
+        const url = 'https://github.com',
+            search = '/';
         if (args.length == 0) redirect(url)
         else redirect(url + search + args.join(''));
     },
 
     // GitHub Gist
     'gist': (args) => {
-        const url = 'https://gist.github.com', search = '/';
+        const url = 'https://gist.github.com',
+            search = '/';
         if (args.length == 0) redirect(url)
         else redirect(url + search + args.join(''));
     },
 
     // Wolfram Alpha
     'wa': (args) => {
-        const url = 'http://wolframalpha.com', search = '/input/?i=';
+        const url = 'http://wolframalpha.com',
+            search = '/input/?i=';
         if (args.length == 0) redirect(url)
         else redirect(buildURL(url, search, args.join(' ')));
     },
 
     // Netflix
     'n': (args) => {
-        const url = 'https://netflix.com', search = '/search?q=';
+        const url = 'https://netflix.com',
+            search = '/search?q=';
         if (args.length == 0) redirect(url)
         else redirect(buildURL(url, search, args.join(' ')));
     },
 
     // Internet Movie Database
     'imdb': (args) => {
-        const url = 'http://imdb.com', search = '/find?s=all&q=';
+        const url = 'http://imdb.com',
+            search = '/find?s=all&q=';
         if (args.length == 0) redirect(url)
         else redirect(buildURL(url, search, args.join(' ')));
     },
 
     // Google Maps
     'map': (args) => {
-        const url = 'https://google.com/maps', search = '/search/';
+        const url = 'https://google.com/maps',
+            search = '/search/';
         if (args.length == 0) redirect(url)
         else redirect(buildURL(url, search, args.join(' ')));
     },
 
     // Google Drive
     'gd': (args) => {
-        const url = 'https://drive.google.com', search = '/drive/search?q=';
+        const url = 'https://drive.google.com',
+            search = '/drive/search?q=';
         if (args.length == 0) redirect(url)
         else redirect(buildURL(url, search, args.join(' ')));
     },
@@ -355,84 +351,128 @@ const commands = {
 
     // Google Images
     'img': (args) => {
-        const url = 'https://google.com', search = '/search?tbm=isch&q=';
-        if (args.length == 0) redirect(url)
-        else redirect(buildURL(url, search, args.join(' ')));
-    },
-
-    // Gmail
-    'gm': (args) => {
-        const url = 'https://mail.google.com', search = '/mail/u/0/#search/';
-        if (args.length == 0) redirect(url)
-        else redirect(buildURL(url, search, args.join(' ')));
-    },
-
-    // Google Keep
-    'k': (args) => {
-        const url = 'https://keep.google.com', search = '/#search/text=';
-        if (args.length == 0) redirect(url)
-        else redirect(buildURL(url, search, args.join(' ')));
-    },
-
-    // Trello
-    'tr': (args) => {
-        const url = 'https://trello.com', search = '/search?q=';
+        const url = 'https://google.com',
+            search = '/search?tbm=isch&q=';
         if (args.length == 0) redirect(url)
         else redirect(buildURL(url, search, args.join(' ')));
     },
 
     // Dictionary
     'dict': (args) => {
-        const url = 'http://dictionary.com', search = '/browse/';
+        const url = 'http://dictionary.com',
+            search = '/browse/';
         if (args.length == 0) redirect(url)
         else redirect(buildURL(url, search, args.join(' ')));
     },
 
     // Thesaurus
     'thes': (args) => {
-        const url = 'http://thesaurus.com', search = '/browse/';
+        const url = 'http://thesaurus.com',
+            search = '/browse/';
         if (args.length == 0) redirect(url)
         else redirect(buildURL(url, search, args.join(' ')));
     },
 
     // Amazon
     'a': (args) => {
-        const url = 'https://amazon.com', search = '/s/?field-keywords=';
-        if (args.length == 0) redirect(url)
-        else redirect(buildURL(url, search, args.join(' ')));
-    },
-
-    // Node package manager
-    'npm': (args) => {
-        const url = 'https://npmjs.org', search = '/search?q=';
-        if (args.length == 0) redirect(url)
-        else redirect(buildURL(url, search, args.join(' ')));
-    },
-
-    // Python package index
-    'pypi': (args) => {
-        const url = 'https://pypi.org', search = '/search/?q=';
+        const url = 'https://amazon.com',
+            search = '/s/?field-keywords=';
         if (args.length == 0) redirect(url)
         else redirect(buildURL(url, search, args.join(' ')));
     },
 
     // Stack Overflow
     'so': (args) => {
-        const url = 'https://stackoverflow.com', search = '/search?q=';
+        const url = 'https://stackoverflow.com',
+            search = '/search?q=';
         if (args.length == 0) redirect(url)
         else redirect(buildURL(url, search, args.join(' ')));
     },
 
     // MDN web docs
     'mdn': (args) => {
-        const url = 'https://developer.mozilla.org', search = '/search?q=';
+        const url = 'https://developer.mozilla.org',
+            search = '/search?q=';
         if (args.length == 0) redirect(url)
         else redirect(buildURL(url, search, args.join(' ')))
-    }
+    },
+
+    // Figma
+    'f': (args) => {
+        const url = 'https://www.figma.com/files/recent',
+            search = '';
+        redirect(url);
+    },
+
+    // Unsplash
+    'us': (args) => {
+        const url = 'https://unsplash.com/',
+            search = 's/photos/';
+        if (args.length == 0) redirect(url)
+        else redirect(buildURL(url, search, args.join(' ')));
+    },
 }
-
-
 
 //========================
 // Bookmarks
 //========================
+
+function displayFolders() {
+    console.log('Folders!');
+}
+
+function displayTags() {
+    console.log('Tags!');
+    var tagList = "";
+
+    for (let i = 0; i < dataBookmarks.tags.length; i++) {
+        const tag = dataBookmarks.tags[i];
+        console.log("tag:", tag);
+
+        tagList += `<div class="tag"><label><input type="checkbox" value="0" class="tag-input"><span>` + tag + `</span></label></div>`
+    }
+    document.getElementById('tag-list').innerHTML = tagList;
+}
+
+function filterTags(data) {
+    var selectedTags = [];
+}
+
+function displayURLs(query) {
+    var urlList = "";
+
+    for (let i = 0; i < dataBookmarks.bookmarks.length; i++) {
+        const title = dataBookmarks.bookmarks[i].title;
+        const url = dataBookmarks.bookmarks[i].url;
+        const tags = dataBookmarks.bookmarks[i].tags;
+        console.log("title:", title, "-url:", url, "-tags:", tags);
+
+        urlList += `
+        <a class="url" href="` + url + `">
+            <img class="url-favicon" src="https://www.google.com/s2/favicons?sz=32&domain_url=` + url + `">` + title + `
+        </a>`
+    }
+    document.getElementById('url-list').innerHTML = urlList;
+}
+
+//========================
+// Modal to add bookmark
+//========================
+
+var modal = document.getElementById("modal-addbookmark");
+var btn = document.getElementById("btn-add");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
